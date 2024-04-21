b0b0655e7bb21ab3135ccbc6f9d3702e9dbad04b
Guilherme Alves Area Matos <drdrakino@gmail.com>
2024-04-21 19:20:42 -0300

    :sparkles: feat: Exercícios 1 e 3

---
+++ Exercises/#1.py
@@ -0,0 +1,15 @@
+#[Ex 1] Calcular Área e Perímetro de um retângulo informando a Base e Altura
+print("-------------------------------------------------------")
+print("Calculadora de Área e Perímetro baseado na Base e Altura")
+print("-------------------------------------------------------\n")
+
+base = int(input("Qual a base de seu retângulo em cm? "))
+
+altura = int(input("Qual a altura de seu retângulo em cm? "))
+
+area = base * altura
+perimetro = (base * 2) + (altura * 2)
+
+print(f"\n A Área de seu retângulo é {area}cm e o perímetro é igual a {perimetro}cm")
+
+
