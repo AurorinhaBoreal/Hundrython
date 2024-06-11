c6d4a2c2c333b6981f6af36ba5facf63ba821ea2
Guilherme Alves Area Matos <drdrakino@gmail.com>
2024-06-11 12:13:23 -0300

    :sparkles: Exercícios 93 - 97

--- Exercises/#93.py
+++ Exercises/#93.py
@@ -1,5 +1,5 @@
 print("--------------------------------------------------------------------")
-print("Matriz")
+print("Diagonaix")
 print("--------------------------------------------------------------------\n")
 
 #Faça um programa em cobra para ler valores e armazená-los em uma matriz D 5 x 5. A seguir
@@ -9,9 +9,27 @@ print("--------------------------------------------------------------------\n")
 #Queria ter prestado atenção nas aulas de matemáticas no Ensino médio....
 
 matrizD = []
-for i in range(1, 3):
+for i in range(5):
     linha = []
-    for y in range(1, 3):
-        d = int(input(f"Digite o número de ordem {y} da lista {i}: "))
+    for y in range(5):
+        d = int(input(f"Digite o número de indíce {y} da lista {i}: "))
         linha.append(d)
     matrizD.append(linha)
+
+def Principal(matriz):
+    soma = 0
+    for i in range(5):
+        soma += matriz[i][i]
+    return soma
+
+def Secuandaria(matriz):
+    soma = 0
+    for i in range(5):
+        soma += matriz[i][4 - i]
+    return soma
+
+prin = Principal(matrizD)
+sec = Secuandaria(matrizD)
+
+print(f"\nA soma da diagonal principal é: {prin}")
+print(f"\nA soma da diagonal secundária é: {sec}")
